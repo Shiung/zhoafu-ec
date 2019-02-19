8607d6d1aed177401225eb6905a0b596614cf140
Jack <shiungchenyu@gmail.com>
2019-02-19 15:06:32 +0800

    loading 主要頁面

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
+    <loading
+      :active.sync="isLoading"
+      color="rgba(36, 94, 96,1)"
+      :opacity= "loadingOpacity"
+      loader="dots"
+      style="z-index:1500;"
+    >
+    </loading>
     <router-view/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      // isLoading: true,
+      loadingOpacity: 0.6
+    }
+  },
+  computed: {
+    ...mapGetters(['isLoading'])
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true, // 嚴格模式 建議在開發時檢查是否依照正常vuex 流程使用(耗效能)
   state: {
-    menuSwitch: false
+    menuSwitch: false,
+    isLoading: false // 會員狀態確認
   },
   actions: {
     menu_status (context, params) {
@@ -16,10 +17,14 @@ export default new Vuex.Store({
   mutations: {
     MENUSWITCH (state, payload) {
       state.menuSwitch = payload
+    },
+    ISLOADING (state, payload) {
+      state.isLoading = payload
     }
   },
   getters: {
-    menuStatus: state => state.menuSwitch
+    menuStatus: state => state.menuSwitch,
+    isLoading: state => state.isLoading
   },
   modules: {
   }
