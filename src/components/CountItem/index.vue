2dd4c2499108c10f6b7c09ea15db28e5c5cd5f07
Jack <shiungchenyu@gmail.com>
2019-02-20 16:22:37 +0800

    數量選擇 元件 fix 2

--- src/components/CountItem/index.vue
+++ src/components/CountItem/index.vue
@@ -16,6 +16,11 @@ export default {
       default: 1
     }
   },
+  data () {
+    return {
+      dataInput: this.count
+    }
+  },
   methods: {
     addCount () {
       if (this.count >= this.max) {
@@ -26,7 +31,7 @@ export default {
         return
       }
       let num = Number(this.count) + 1
-      this.$emit('increment', num)
+      this.emitData(num)
     },
     minusCount () {
       if (this.count <= this.min) {
@@ -37,8 +42,33 @@ export default {
         return
       }
       let num = Number(this.count) - 1
-      this.$emit('decrement', num)
+      this.emitData(num)
       // this.countNum--
+    },
+    change () {
+      // console.log('input 有改變')
+      let input = Number(this.dataInput)
+      let max = this.max
+      let min = this.min
+      if (input <= max && input >= min && Number.isInteger(input)) {
+        this.emitData(input)
+      } else if (input === 0) { // 沒輸入值或是輸入 1-2 或是輸入 0
+        // console.log('沒輸入數值')
+      } else {
+        // console.log('數值超出範圍')
+        // console.log(Number.isInteger(input))
+        // console.log(input)
+        this.dataInput = this.count
+      }
+    },
+    emitData (num) {
+      this.$emit('changeData', num)
+    }
+  },
+  watch: {
+    count () {
+      // console.log('props 有變化')
+      this.dataInput = this.count
     }
   }
 }
